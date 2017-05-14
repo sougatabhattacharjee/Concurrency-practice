bf4256a77136fc7d69c0a8e2f675c7cab42e8187
sauravbiswasiupr <sauravbiswasiupr@github.com>
2017-05-14 01:08:46 -0700

    Some changes to the script

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
 
 for ((i=1;i<=10;i++)); do
   echo "Thread Count: ${i}";
-  java -jar target/ThumbnailMaker.jar --source /Users/saurav/Desktop/ --output /Users/saurav/Desktop/Personal_Projects/thumbnails/ --thread ${i} |  grep "^Total.*" | awk '{print $11}' >> /var/tmp/times.log 
+  java -jar target/ThumbnailMaker.jar --source $SOURCE_DIR --output $DESTINATION_DIR --thread ${i} |  grep "^Total.*" | awk '{print $11}' >> /var/tmp/times.log 
 done
